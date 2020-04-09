c13c2869bab880cd5dbc3918954d2078e3686370
Jose <joserprg@gmail.com>
2020-04-08 21:23:35 -0400

    Minor code changes

--- gui/zfs_bootenv_maintain_gui.php
+++ gui/zfs_bootenv_maintain_gui.php
@@ -228,7 +228,7 @@ $document->render();
 					<td class="vncellt"><?=gtext("Extension version");?></td>
 					<td class="vtable"><span name="getinfo_ext" id="getinfo_ext"><?=get_version_ext()?></span></td>
 				</tr>
-				<?php html_filechooser("backup_path", gtext("Backup directory"), $backup_path, gtext("Directory to store Boot Environments archive.zfs files, use as file chooser for restoring from file."), $backup_path, true, 60);?>
+				<?php html_filechooser("backup_path", gtext("Backup directory"), $backup_path, gtext("Directory to store Boot Environments .xz archives, use as file chooser for restoring from file."), $backup_path, true, 60);?>
 			</table>
 			<div id="submit">
 				<input id="save" name="save" type="submit" class="formbtn" title="<?=gtext("Save settings");?>" value="<?=gtext("Save");?>"/>
